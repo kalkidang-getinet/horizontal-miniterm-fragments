180edc60ea2a09788bb8a70d9116415b64963cbd
kalkidange <halle2019newon22@gmail.com>
2024-02-26 10:46:31 +0300

    horzontal fragment

--- Example.py
+++
@@ -1,9 +0,0 @@
-from hor_fragment import * 
-
-relations = ["customer", "order", "product"]
-predicates = [("customer", "placed_order", "order"), ("order", "includes", "product")]
-print("work hear")
-generator = HorizontalMinitermFragmentGenerator(relations)
-print("hear too")
-fragments = generator.generate_fragments(predicates)
-print(fragments)
